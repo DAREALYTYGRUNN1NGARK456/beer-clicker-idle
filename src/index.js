d29888b65738744b98a7590da174f7fd9ce96467
John Vidal <ba.john.vidal@gmail.com>
2020-10-01 18:05:29 -0300

    basic upgrade status added

--- src/index.js
+++ src/index.js
@@ -7,16 +7,79 @@ const CPSpan = document.getElementById("CPSpan");
 let credits = 0,
   cps = 0;
 
-const upgrades = [
-  { name: "upgradeOne", price: 10, cps: 1 },
-  { name: "upgradeTwo", price: 100, cps: 10 },
+const UpgradesList = [];
+const BuildsList = [
+  {
+    id: 1,
+    name: "Upgrade one",
+    price: 10,
+    cps: 1,
+    upgrades: [
+      { id: 11, name: "1 - Upgrade one", price: 100, bought: false },
+      { id: 12, name: "1 - Upgrade two", price: 1000, bought: false },
+    ],
+  },
+  {
+    id: 2,
+    name: "Upgrade two",
+    price: 100,
+    cps: 10,
+    upgrades: [
+      { id: 21, name: "2 - Upgrade one", price: 100, bought: false },
+      { id: 22, name: "2 - Upgrade two", price: 1000, bought: false },
+    ],
+  },
 ];
 
-function CalculateUpgrade() {
-  const upgradesEnables = upgrades.filter((upgrade) => upgrade.price < credits);
+function createUpgradeButtons() {
+  const btnUpgradesList = [];
+
+  if (UpgradesList.length === 0) {
+    BuildsList.forEach((build) => {
+      UpgradesList.push.apply(UpgradesList, build.upgrades);
+    });
+  }
+
+  UpgradesList.forEach((upgrades) => {
+    if (upgrades.bought) return;
+
+    btnUpgradesList.push(
+      `<button id="${upgrades.id}" onclick="BuyUpgrade(this.id)" disabled>${upgrades.name}</button>`
+    );
+  });
+
+  document.getElementById("upgradelist").innerHTML = "";
+  document.getElementById("upgradelist").innerHTML = `${btnUpgradesList.join(
+    ""
+  )}`;
+}
+
+function CreateGamePage() {
+  const btnList = [];
+
+  BuildsList.forEach((build) => {
+    btnList.push(
+      `<button id="${build.id}" onclick="BuyBuild(this.id)" disabled>${build.name}</button>`
+    );
+  });
+
+  createUpgradeButtons();
+
+  document.getElementById("buildinglist").innerHTML = `${btnList.join("")}`;
+}
+
+function CalculateStoreStatus() {
+  const buildsEnables = BuildsList.filter((build) => build.price < credits);
+
+  buildsEnables.forEach((build) => {
+    document.getElementById(build.id).disabled = false;
+  });
+
+  const upgradesEnables = UpgradesList.filter((build) => build.price < credits);
 
   upgradesEnables.forEach((upgrade) => {
-    document.getElementById(upgrade.name).disabled = false;
+    if (upgrade.bought) return;
+    document.getElementById(upgrade.id).disabled = false;
   });
 }
 
@@ -24,9 +87,23 @@ function ClickOnTarget() {
   credits += 1;
 }
 
-function Upgrade(id) {
-  const currentUpgrade = upgrades.find((upgrade) => id === upgrade.name);
-  cps += currentUpgrade.cps;
+function BuyBuild(id) {
+  const currentBuild = BuildsList.find((build) => Number(id) === build.id);
+  credits -= currentBuild.price;
+  cps += currentBuild.cps;
+}
+
+function BuyUpgrade(id) {
+  const currentUpgrade = UpgradesList.find(
+    (upgrade) => Number(id) === upgrade.id
+  );
+
+  const index = UpgradesList.findIndex((obj) => obj.id === currentUpgrade.id);
+
+  UpgradesList.splice(index, 1, { ...currentUpgrade, bought: true });
+
+  credits -= currentUpgrade.price;
+  createUpgradeButtons();
 }
 
 function Draw() {
@@ -36,7 +113,9 @@ function Draw() {
 }
 
 var mainloop = function () {
-  CalculateUpgrade(), Draw();
+  CalculateStoreStatus(), Draw();
 };
-
-window.onload = () => setInterval(mainloop, 16);
+window.onload = () => {
+  CreateGamePage();
+  setInterval(mainloop, 16);
+};
